eb92b3e63d50399ae3956bdf9d840895529e542a
JoelScarinius <anjo19go@student.ju.se>
2023-09-08 12:19:52 +0200

    ADD: Initialize React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./components";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <video
+                src="../../src/video/vecteezy_on-a-black-background-yellow-smoke-spread-like-a-fire_2017728.mp4"
+                autoplay
+                loop
+                muted
+            ></video>
+            <h1>Welcome to our meeting scheduling site!</h1>
+
+            <h3>
+                <p id="typing-element"></p>
+            </h3>
+
+            <h2>Please Login or Sign up here!</h2>
+            <div class="login_area">
+                <a href="register.html">Click here to register!</a>
+                <a href="login.html">Click here to login!</a>
+            </div>
+        </div>
+    );
 }
 
 export default App;

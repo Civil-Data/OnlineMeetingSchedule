373aa0c8cdc794db8a3c90f91e8b246f9cb3ece0
JoelScarinius <anjo19go@student.ju.se>
2023-09-11 10:08:39 +0200

    ADD: typing effect to login and booking. Deleted some unnessacary files and imports

--- frontend/src/views/Booking.js
+++ frontend/src/views/Booking.js
@@ -1,8 +1,15 @@
-import React from "react";
-import "../css/App.css";
+import TypingEffect from "../Components/TypingEffect";
 
 const Booking = () => {
-    return <h1>Booking page</h1>;
+    return (
+        <>
+            <h1>Booking page</h1>;
+            <TypingEffect
+                text="Welcome to the booking page! Please enter your name and email to book a meeting."
+                delay={25}
+            />
+        </>
+    );
 };
 
 export default Booking;

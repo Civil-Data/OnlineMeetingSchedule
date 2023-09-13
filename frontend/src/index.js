19b93d8fec054a7623e0fd61f273c9d776ffd1f0
Martin Nilsson <martin.no.nilsson@gmail.com>
2023-09-13 21:35:50 +0200

    ADD: Implemented Profile page into the app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,16 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 // import "./css/index.css";
 import "./css/App.css";
-// import App from "./App";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Profile from "./views/Profile";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            {/* <App /> */}
-            <Profile />
+            <App />
         </BrowserRouter>
     </React.StrictMode>
 );

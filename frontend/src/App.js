25c9484099560fcd0cf996fe3ae1376c717652ee
JoelScarinius <anjo19go@student.ju.se>
2023-09-21 11:55:00 +0200

    FEAT: Show logged in account(in progress)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,10 @@ import { Routes, Route } from "react-router-dom";
 import "./css/App.css";
 import Home from "./views/Home";
 import Register from "./views/Register";
-import Login from "./views/Login";
 import Booking from "./views/Booking";
 import NavigationBar from "./Components/NavigationBar";
 import { ProfileProvider } from "./contexts/ProfileContext";
+import { LoginProvider } from "./contexts/LoginContext";
 
 function App() {
     return (
@@ -14,10 +14,10 @@ function App() {
             <NavigationBar />
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
                 <Route path="/booking" element={<Booking />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/profile" element={<ProfileProvider />} />
+                <Route path="/login" element={<LoginProvider />} />
             </Routes>
         </>
     );

7672f9e93f70c4daef5fa130a2bacf9c13b83a7e
matildaronder <matilda@ronder.se>
2023-09-21 11:55:17 +0200

    FIXED: Pop-up window

--- frontend/src/Components/DateButtons.js
+++ frontend/src/Components/DateButtons.js
@@ -1,9 +1,29 @@
 import React, { useState } from "react";
 import ConfirmButton from "./ConfirmButton";
-// import Confirmation from "./Confirmation";
+import ClearIcon from "@mui/icons-material/Clear";
+// import { Link, useNavigate } from "react-router-dom";
 
-const DateButtons = ({ date, confirm }) => {
+const DateButtons = ({ date }) => {
 	const [dayView, setDayView] = useState(false);
+	// const [time, setTime] = useState(false);
+	// const timeSlots = [
+	// 	{ time: "8:00" },
+	// 	{ time: "9:00" },
+	// 	{ time: "10:30" },
+	// 	{ time: "12:00" },
+	// 	{ time: "14:45" },
+	// ];
+	// const [selectedTime, setSelectedTime] = useState(null);
+
+	// const handleTimeClick = (time) => {
+	// 	setSelectedTime(time === selectedTime ? null : time);
+	// };
+	// const navigate = useNavigate();
+
+	// const handleClearClick = () => {
+	// 	// Go back to the previous route
+	// 	navigate("/booking");
+	// };
 
 	// const dateLabels = Array.from({ length: 31 }, (_, index) => index + 1);
 	// function dayOverview() {}
@@ -34,12 +54,14 @@ const DateButtons = ({ date, confirm }) => {
 			{dayView && (
 				<div className="viewShadow">
 					<div className="dayView">
+						<ClearIcon></ClearIcon>
+
 						<div className="calendarDate">Monday {date}</div>
 						<div className="time">
 							<button>8:00</button>
 							<button>9:00</button>
-							<button>10:30</button>
-							<button>12:00</button>
+							<button>11:30</button>
+							<button>13:00</button>
 							<button>14:45</button>
 						</div>
 						<ConfirmButton></ConfirmButton>
--- frontend/src/Components/NavigationBar.js
+++ frontend/src/Components/NavigationBar.js
@@ -4,41 +4,41 @@ import AddIcon from "@mui/icons-material/Add";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 const NavigationBar = () => {
-    return (
-        <div id="navigationBar">
-            <div id="icons" className="flex-se">
-                <Link className="nav-buttons" to="/" type="button">
-                    <HomeIcon
-                        fontSize="large"
-                        sx={{ color: "#daa520" }}
-                        titleAccess="Home Page"
-                    />
-                </Link>
-                <Link className="nav-buttons" to="/booking" type="button">
-                    <AddIcon
-                        fontSize="large"
-                        sx={{ color: "#daa520" }}
-                        titleAccess="Add Booking"
-                    />
-                </Link>
-                <Link className="nav-buttons" to="/profile" type="button">
-                    <AccountCircleIcon
-                        fontSize="large"
-                        sx={{ color: "#daa520" }}
-                        titleAccess="Profile Page"
-                    />
-                </Link>
-            </div>
-            <div id="navButtons" className="flex-se">
-                <Link className="nav-buttons" to="/register" type="button">
-                    <i>Sign up</i>
-                </Link>
-                <Link className="nav-buttons" to="/login" type="button">
-                    <span>Login</span>
-                </Link>
-            </div>
-        </div>
-    );
+	return (
+		<div id="navigationBar">
+			<div id="icons" className="flex-se">
+				<Link className="nav-buttons" to="/" type="button">
+					<HomeIcon
+						fontSize="large"
+						sx={{ color: "#daa520" }}
+						titleAccess="Home Page"
+					/>
+				</Link>
+				<Link className="nav-buttons" to="/booking" type="button">
+					<AddIcon
+						fontSize="large"
+						sx={{ color: "#daa520" }}
+						titleAccess="Add Booking"
+					/>
+				</Link>
+				<Link className="nav-buttons" to="/profile" type="button">
+					<AccountCircleIcon
+						fontSize="large"
+						sx={{ color: "#daa520" }}
+						titleAccess="Profile Page"
+					/>
+				</Link>
+			</div>
+			<div id="navButtons" className="flex-se">
+				<Link className="nav-buttons" to="/register" type="button">
+					<i>Sign up</i>
+				</Link>
+				<Link className="nav-buttons" to="/login" type="button">
+					<span>Login</span>
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default NavigationBar;

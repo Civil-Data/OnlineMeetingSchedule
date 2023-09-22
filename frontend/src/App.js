4b18f9311e17f42b55d61269b5fc171d21c6c1de
Martin Nilsson <martin.no.nilsson@gmail.com>
2023-09-22 11:42:09 +0200

    FIX: Popup for booking by implementing context provider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,25 +3,25 @@ import { Routes, Route } from "react-router-dom";
 import "./css/App.css";
 import Home from "./views/Home";
 import Register from "./views/Register";
-import Booking from "./views/Booking";
+// import Booking from "./views/Booking";
 import NavigationBar from "./Components/NavigationBar";
 import { ProfileProvider } from "./contexts/ProfileContext";
 import Login from "./views/Login";
-// import { BookingProvider } from "./contexts/BookingContext";
+import { BookingProvider } from "./contexts/BookingContext";
 
 function App() {
-	return (
-		<>
-			<NavigationBar />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/booking" element={<Booking />} />
-				<Route path="/register" element={<Register />} />
-				<Route path="/profile" element={<ProfileProvider />} />
-			</Routes>
-		</>
-	);
+    return (
+        <>
+            <NavigationBar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/booking" element={<BookingProvider />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/profile" element={<ProfileProvider />} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
--- frontend/src/Components/DateButtons.js
+++ frontend/src/Components/DateButtons.js
@@ -1,74 +1,41 @@
-import React, { useState } from "react";
-import ConfirmButton from "./ConfirmButton";
-import ClearIcon from "@mui/icons-material/Clear";
-// import { Link, useNavigate } from "react-router-dom";
+import React from "react";
+import { useDayViewUpdate } from "../contexts/BookingContext";
 
 const DateButtons = ({ date, confirm, theme }) => {
-	const [dayView, setDayView] = useState(false);
+    const { openDayView } = useDayViewUpdate();
 
-	// const dateLabels = Array.from({ length: 31 }, (_, index) => index + 1);
-	// function dayOverview() {}
-	// useEffect(() => {}, [dayView]);
-	// const getDayOfWeek = (dateString) => {
-	// 	const daysOfWeek = [
-	// 		"Monday",
-	// 		"Tuesday",
-	// 		"Wednesday",
-	// 		"Thursday",
-	// 		"Friday",
-	// 		"Saturday",
-	// 		"Sunday",
-	// 	];
-	// 	const date = new Date(dateString);
-	// 	const dayOfWeekIndex = date.getDay();
-	// 	return daysOfWeek[dayOfWeekIndex];
-	// };
+    // # TODO: FIGURE OUT HOW TO GET GRID COLUMN INDEX AND PASS IT AS PROP TO DATEBUTTON COMPONENT. #
+    // # THEN GET THE CORRECT DAY BY THAT INDEX #
 
-	function closeDayView() {
-		// console.log(bool);
-		setDayView(false);
-	}
-
-	function openDayView() {
-		setDayView(true);
-	}
-	return (
-		<div
-			key={date}
-			className={`dates ${theme === "grey" ? "grey_dates" : ""}`}
-			onClick={() => {
-				// openDayView();
-				openDayView();
-			}}
-		>
-			{date}
-			<div className="date_info">Meeting 9:00am</div>
-			{dayView && (
-				<div className="viewShadow">
-					<div className="dayView">
-						<div
-							onClick={() => {
-								closeDayView();
-							}}
-						>
-							<ClearIcon />
-						</div>
-						<div className="calendarDate">Monday {date}</div>
-						<div className="time">
-							<button>8:00</button>
-							<button>9:00</button>
-							<button>10:30</button>
-							<button>12:00</button>
-							<button>14:45</button>
-						</div>
-						<ConfirmButton
-							closePopup={closeDayView}
-						></ConfirmButton>
-					</div>
-				</div>
-			)}
-		</div>
-	);
+    // const dateLabels = Array.from({ length: 31 }, (_, index) => index + 1);
+    // function dayOverview() {}
+    // useEffect(() => {}, [dayView]);
+    // const getDayOfWeek = (dateString) => {
+    // 	const daysOfWeek = [
+    // 		"Monday",
+    // 		"Tuesday",
+    // 		"Wednesday",
+    // 		"Thursday",
+    // 		"Friday",
+    // 		"Saturday",
+    // 		"Sunday",
+    // 	];
+    // 	const date = new Date(dateString);
+    // 	const dayOfWeekIndex = date.getDay();
+    // 	return daysOfWeek[dayOfWeekIndex];
+    // };
+    return (
+        <div
+            key={date}
+            className={`dates ${theme === "grey" ? "grey_dates" : ""}`}
+            onClick={() => {
+                openDayView(date);
+            }}
+        >
+            {date}
+            <div className="date_info">Meeting 9:00am</div>
+        </div>
+    );
 };
 
 export default DateButtons;

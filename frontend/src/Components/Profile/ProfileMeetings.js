06529ab52f8a5f61870a16b49d9d722c847f30cf
JoelScarinius <anjo19go@student.ju.se>
2023-09-26 13:10:55 +0200

    FIX: Merge conflicts

--- frontend/src/Components/Profile/ProfileMeetings.js
+++ frontend/src/Components/Profile/ProfileMeetings.js
@@ -1,77 +1,76 @@
 import React from "react";
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
-import { useTabContext, useProfileUpdate } from "../../contexts/ProfileContext";
+import { useProfileUpdate } from "../../contexts/ProfileContext";
 
 // import PopUp from "../PopUp";
 
 const ProfileMeetings = () => {
-	const tabContext = useTabContext();
-	const { updateMyMeetings, details } = useProfileUpdate();
+    const { updateMyMeetings, details } = useProfileUpdate();
 
-	return (
-		<>
-			<div className="profile_container">
-				<div className="top_section">
-					{/* {tabContext === "myMeetings" && ( */}
-					<>
-						{details ? (
-							<div className="meeting_list">
-								<div className="meeting_list_item  ">
-									<div className="meeting_list_sub_item">
-										Event name
-									</div>
-									<div className="meeting_list_sub_item">
-										When?
-									</div>
-									<div className="meeting_list_sub_item">
-										Where?
-									</div>
-								</div>
-								<div
-									className="details"
-									style={{ cursor: "pointer" }}
-									onClick={() => {
-										updateMyMeetings(false);
-									}}
-								>
-									<ArrowRightIcon />
-									Details
-								</div>
-								<div className="participants">
-									Participants: Martin, Joel, Felix, Matilda
-								</div>
-							</div>
-						) : (
-							<div className="meeting_list">
-								<div className="meeting_list_item  ">
-									<div className="meeting_list_sub_item">
-										Event name
-									</div>
-									<div className="meeting_list_sub_item">
-										When?
-									</div>
-									<div className="meeting_list_sub_item">
-										Where?
-									</div>
-								</div>
-								<div
-									className="details"
-									style={{ cursor: "pointer" }}
-									onClick={() => {
-										updateMyMeetings(true);
-									}}
-								>
-									<ArrowRightIcon />
-									Details
-								</div>
-							</div>
-						)}
-					</>
-					{/* )} */}
-				</div>
-			</div>
-		</>
-	);
+    return (
+        <>
+            <div className="profile_container">
+                <div className="top_section">
+                    {/* {tabContext === "myMeetings" && ( */}
+                    <>
+                        {details ? (
+                            <div className="meeting_list">
+                                <div className="meeting_list_item  ">
+                                    <div className="meeting_list_sub_item">
+                                        Event name
+                                    </div>
+                                    <div className="meeting_list_sub_item">
+                                        When?
+                                    </div>
+                                    <div className="meeting_list_sub_item">
+                                        Where?
+                                    </div>
+                                </div>
+                                <div
+                                    className="details"
+                                    style={{ cursor: "pointer" }}
+                                    onClick={() => {
+                                        updateMyMeetings(false);
+                                    }}
+                                >
+                                    <ArrowRightIcon />
+                                    Details
+                                </div>
+                                <div className="participants">
+                                    Participants: Martin, Joel, Felix, Matilda
+                                </div>
+                            </div>
+                        ) : (
+                            <div className="meeting_list">
+                                <div className="meeting_list_item  ">
+                                    <div className="meeting_list_sub_item">
+                                        Event name
+                                    </div>
+                                    <div className="meeting_list_sub_item">
+                                        When?
+                                    </div>
+                                    <div className="meeting_list_sub_item">
+                                        Where?
+                                    </div>
+                                </div>
+                                <div
+                                    className="details"
+                                    style={{ cursor: "pointer" }}
+                                    onClick={() => {
+                                        updateMyMeetings(true);
+                                    }}
+                                >
+                                    <ArrowRightIcon />
+                                    Details
+                                </div>
+                            </div>
+                        )}
+                    </>
+                    {/* )} */}
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default ProfileMeetings;

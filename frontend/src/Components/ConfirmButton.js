918182adca7c8912722c8de5b848230b6f4083cd
Stocken99 <felix.stockinger@yahoo.se>
2023-09-28 15:48:36 +0200

    started work on input validation for booking of meetings

--- frontend/src/Components/ConfirmButton.js
+++ frontend/src/Components/ConfirmButton.js
@@ -3,22 +3,22 @@ import Confirmation from "./Confirmation";
 import { useDayViewUpdate } from "../contexts/BookingContext";
 function ConfirmButton({ selectedTime }) {
     const [showComponent, setShowComponent] = useState(false);
-    const [buttonText, setButtonText] = useState("Confirm booking");
+    const [buttonText, setButtonText] = useState("Send Invite");
     const { closeDayView } = useDayViewUpdate();
 
-    const toggleComponent = e => {
+    const toggleComponent = (e) => {
         const text = e.target.innerText;
         if (text === "Back to booking") {
             closeDayView();
         } else {
-            setShowComponent(prevState => !prevState);
+            setShowComponent((prevState) => !prevState);
             setButtonText("Back to booking");
         }
     };
 
     return (
         <>
-            <button id="confirmation_btn" onClick={e => toggleComponent(e)}>
+            <button id="confirmation_btn" onClick={(e) => toggleComponent(e)}>
                 {buttonText}
             </button>
             {showComponent && <Confirmation />}

9c3299dce630328a738a06bf1afa9125e49a482b
Martin Nilsson <martin.no.nilsson@gmail.com>
2023-09-29 13:09:25 +0200

    Soon done with calender rendering

--- frontend/src/views/Booking.js
+++ frontend/src/views/Booking.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState, useCallback } from "react";
 // import React, { useState } from "react";
 import TypingEffect from "../Components/TypingEffect";
 
@@ -11,117 +11,174 @@ import { useDayView } from "../contexts/BookingContext";
 import { v4 as uuidv4 } from "uuid";
 import { useDateContext } from "../contexts/DateContext";
 import Dropdown from "../Components/Booking/Dropdown";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 const Booking = () => {
-	const { dayView, date, dayString } = useDayView();
-	const { getDate } = useDateContext();
-	// console.log(getDate);
+    const { dayView, date, dayString } = useDayView();
+    const { getDate } = useDateContext();
+    // console.log(getDate);
 
-	// const [monthToDisplay, setMonthToDisplay] = useState(getDate.month);
+    const [monthToDisplay, setMonthToDisplay] = useState(getDate().month);
+    const [yearToDisplay, setYearToDisplay] = useState(getDate().year);
+    const [monthString, setMonthString] = useState(
+        getDate(yearToDisplay, monthToDisplay).monthString
+    );
+    // const [dateObj, setDateObj] = useState();
+    const [dateLabels, setDateLabels] = useState([]);
 
-	const dateLabels = [];
-	// const dates = 31;
-	const dateObj = getDate(2023, 9, 0);
-	console.log(dateObj);
-	// const month = dateObj.month + 1;
-	// const year = dateObj.year;
-	const dates = dateObj.daysInMonth;
-	// const dates = getDaysInMonth(year, month);
-	console.log(dates);
-	let bgd = "dark";
-	let dateIdx = 0;
-	for (let index = 0; index < 35; index++) {
-		let dateNum = dateIdx + 1;
-		if (dateNum > dates) {
-			bgd = "grey";
-			dateIdx = 0;
-			dateNum = 1;
-		}
-		dateIdx++;
+    // let dateObj;
+    function updateMonth(monthStep) {
+        if (monthToDisplay === 0 && monthStep < 0) {
+            setYearToDisplay(year => year - 1);
+            setMonthToDisplay(11);
+        } else if (monthToDisplay === 11 && monthStep > 0) {
+            setYearToDisplay(year => year + 1);
+            setMonthToDisplay(0);
+        } else {
+            setMonthToDisplay(month => month + monthStep);
+        }
+    }
 
-		dateLabels.push(
-			<DateButtons
-				key={uuidv4()}
-				date={dateNum}
-				dayString={
-					getDate(dateObj.year, dateObj.month, dateNum).dayString
-				}
-				theme={bgd}
-			/>
-		);
-	}
+    // const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
-	const selectOptions = [
-		{
-			labelText: "Please choose lenght:",
-			options: ["30 min", "45 min", "1h", "2h"],
-		},
-		{
-			labelText: "Please choose person:",
-			options: ["Martin", "Joel", "Matilda", "Felix"],
-		},
-		{
-			labelText: "Filter participants by country:",
-			options: ["Sweden", "Norway", "Denmark", "Finland"],
-		},
-	];
+    const renderDates = useCallback(() => {
+        // function renderDates() {
+        const dateObj = getDate(yearToDisplay, monthToDisplay);
+        // setDateObj(dateObj);
+        setMonthString(dateObj.monthString);
+        // const dates = 31;
+        // console.log(dateObj);
+        console.log(dateObj);
+        // const month = dateObj.month + 1;
+        // const year = dateObj.year;
+        const dates = dateObj.daysInMonth;
+        // const startDay = dateObj.startDayOfMonth;
+        // let startOfGreyDays;
+        // for (let i = 0; i < days.length; i++) {
+        //     if (startDay === days[i]) {
+        //         // startOfGreyDays = i;
+        //     }
+        // }
 
-	return (
-		<>
-			{dayView && (
-				<>
-					<PopUp>
-						<div className="calendarDate">
-							{dayString} {date}
-						</div>
-						<div className="time">
-							<button>8:00</button>
-							<button>9:00</button>
-							<button>10:30</button>
-							<button>12:00</button>
-							<button>14:45</button>
-							<ConfirmButton></ConfirmButton>
-						</div>
-					</PopUp>
-				</>
-			)}
+        // for (let i = 0; i < startOfGreyDays; i++) {
 
-			<div>
-				<div className="calender_area">
-					<div className="titles">
-						<h1>Booking Page</h1>
-						<TypingEffect
-							text="Welcome to the booking page! Please choose length of the meeting and choose a valid day to book a meeting."
-							delay={25}
-						/>
-					</div>
-					<div className="meeting_options">
-						{selectOptions.map((dropdown) => {
-							return (
-								<Dropdown
-									key={uuidv4()}
-									id={selectOptions.indexOf(dropdown)}
-									labelText={dropdown.labelText}
-									selectOptions={dropdown.options}
-								/>
-							);
-						})}
-					</div>
-					<div className="grid-container">
-						<div className="month">{dateObj.monthString}</div>
-						<div className="day">Monday</div>
-						<div className="day">Tuesday</div>
-						<div className="day">Wednesday</div>
-						<div className="day">Thursday</div>
-						<div className="day">Friday</div>
-						<div className="day">Saturday</div>
-						<div className="day">Sunday</div>
-						{dateLabels}
-					</div>
-				</div>
-			</div>
-		</>
-	);
+        // }
+
+        // const dates = getDaysInMonth(year, month);
+        const dateLabels = [];
+        let dateIdx = 0;
+        for (let index = 0; index < 35; index++) {
+            let bgd = "dark";
+            let dateNum = dateIdx + 1;
+            if (dateNum > dates) {
+                bgd = "grey";
+                dateIdx = 0;
+                dateNum = 1;
+            }
+            dateIdx++;
+
+            // ...prevDatelbl,
+            dateLabels.push(
+                <DateButtons
+                    key={uuidv4()}
+                    date={dateNum}
+                    dayString={getDate(dateObj.year, dateObj.month, dateNum).dayString}
+                    theme={bgd}
+                />
+            );
+        }
+        setDateLabels(dateLabels);
+        // }
+    }, [getDate, monthToDisplay, yearToDisplay]);
+
+    const selectOptions = [
+        {
+            labelText: "Please choose lenght:",
+            options: ["30 min", "45 min", "1h", "2h"],
+        },
+        {
+            labelText: "Please choose person:",
+            options: ["Martin", "Joel", "Matilda", "Felix"],
+        },
+        {
+            labelText: "Filter participants by country:",
+            options: ["Sweden", "Norway", "Denmark", "Finland"],
+        },
+    ];
+
+    useEffect(() => {
+        renderDates();
+    }, [monthToDisplay, renderDates]);
+
+    // const arrowStlye = {};
+
+    return (
+        <>
+            {dayView && (
+                <>
+                    <PopUp>
+                        <div className="calendarDate">
+                            {dayString} {date}
+                        </div>
+                        <div className="time">
+                            <button>8:00</button>
+                            <button>9:00</button>
+                            <button>10:30</button>
+                            <button>12:00</button>
+                            <button>14:45</button>
+                            <ConfirmButton></ConfirmButton>
+                        </div>
+                    </PopUp>
+                </>
+            )}
+
+            <div>
+                <div className="calender_area">
+                    <div className="titles">
+                        <h1>Booking Page</h1>
+                        <TypingEffect
+                            text="Welcome to the booking page! Please choose length of the meeting and choose a valid day to book a meeting."
+                            delay={25}
+                        />
+                    </div>
+                    <div className="meeting_options">
+                        {selectOptions.map(dropdown => {
+                            return (
+                                <Dropdown
+                                    key={uuidv4()}
+                                    id={selectOptions.indexOf(dropdown)}
+                                    labelText={dropdown.labelText}
+                                    selectOptions={dropdown.options}
+                                />
+                            );
+                        })}
+                    </div>
+                    <div className="grid-container">
+                        <div className="month">
+                            <div className="arrowBox" onClick={() => updateMonth(-1)}>
+                                <ArrowBackIosNewIcon sx={{ cursor: "pointer" }} />
+                            </div>
+                            <div style={{ width: "300px" }}>
+                                {monthString} {yearToDisplay}
+                            </div>
+                            <div className="arrowBox" onClick={() => updateMonth(1)}>
+                                <ArrowForwardIosIcon sx={{ cursor: "pointer" }} />
+                            </div>
+                        </div>
+                        <div className="day">Monday</div>
+                        <div className="day">Tuesday</div>
+                        <div className="day">Wednesday</div>
+                        <div className="day">Thursday</div>
+                        <div className="day">Friday</div>
+                        <div className="day">Saturday</div>
+                        <div className="day">Sunday</div>
+                        {dateLabels}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Booking;

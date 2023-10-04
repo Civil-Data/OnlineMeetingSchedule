77d19d51cc2708bce3c5c5f8fdb71dec9d8b4f53
matildaronder <matilda@ronder.se>
2023-10-04 14:45:37 +0200

    ADD: Contact-list

--- frontend/src/Components/Profile/ProfileContacts.js
+++ frontend/src/Components/Profile/ProfileContacts.js
@@ -1,30 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios"; // You may need to install axios if you haven't already
+import serverUrl from "../../utils/config";
 
 const ProfileContacts = () => {
-    return (
-        <div>
-            <ul className="contact_list">
-                <li className="list_item">
-                    <span>Joel Scarinius Stävmo</span>
-                    <div className="button_area">
-                        <button>Invite</button>
-                    </div>
-                </li>
-                <li className="list_item">
-                    <span>Matilda Ronder</span>
-                    <div className="button_area">
-                        <button>Invite</button>
-                    </div>
-                </li>
-                <li className="list_item">
-                    <span>Felix Stockinger</span>
-                    <div className="button_area">
-                        <button>Invite</button>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    );
+	const [users, setUsers] = useState([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const res = await axios.get(serverUrl + "/users");
+				setUsers(res.data);
+			} catch (error) {
+				console.error("Error fetching user data:", error);
+			}
+		};
+
+		fetchData();
+	}, []);
+
+	return (
+		<div>
+			{/* <h1>Contact List</h1> */}
+			<ul className="contact_list">
+				{users.map((user, index) => (
+					<li className="list_item" key={index}>
+						<span>{user.name}</span>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 };
 
 export default ProfileContacts;

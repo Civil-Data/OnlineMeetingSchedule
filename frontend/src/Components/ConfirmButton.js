4ac584d3e1ac45bf2acb0c7cfbded324817a49af
matildaronder <matilda@ronder.se>
2023-10-05 15:42:02 +0200

    Resolved merge conflict

--- frontend/src/Components/ConfirmButton.js
+++ frontend/src/Components/ConfirmButton.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Confirmation from "./Confirmation";
 import { useDayViewUpdate } from "../contexts/BookingContext";
 
-<<<<<<< HEAD
 //Component for Confirm button
 function ConfirmButton({ selectedTime }) {
 	const [showComponent, setShowComponent] = useState(false);
@@ -29,35 +28,6 @@ function ConfirmButton({ selectedTime }) {
 			{showComponent && <Confirmation />}
 		</>
 	);
-=======
-function ConfirmButton({ isDisabled }) {
-    const [showComponent, setShowComponent] = useState(false);
-    const [buttonText, setButtonText] = useState("Send Invite");
-    const { closeDayView } = useDayViewUpdate();
-
-    const toggleComponent = (e) => {
-        const text = e.target.innerText;
-        if (text === "Back to booking") {
-            closeDayView();
-        } else {
-            setShowComponent((prevState) => !prevState);
-            setButtonText("Back to booking");
-        }
-    };
-
-    return (
-        <>
-            <button
-                disabled={isDisabled}
-                id="confirmation_btn"
-                onClick={(e) => toggleComponent(e)}
-            >
-                {buttonText}
-            </button>
-            {showComponent && <Confirmation />}
-        </>
-    );
->>>>>>> origin
 }
 
 export default ConfirmButton;

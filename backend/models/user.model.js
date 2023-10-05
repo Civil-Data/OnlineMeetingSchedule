c99aeb20cbb8ab63af878a6dc04d945c8fed7d0f
matildaronder <matilda@ronder.se>
2023-10-05 09:54:08 +0200

    Updated contact view

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,44 +2,44 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "Your name is required"],
-    },
-    email: {
-        type: String,
-        required: [true, "Your email address is required"],
-        unique: true,
-    },
-    // username: {
-    //     type: String,
-    //     required: [true, "Your username is required"],
-    // },
-    password: {
-        type: String,
-        required: [true, "Your password is required"],
-    },
-    telephone: {
-        type: String,
-    },
-    gender: {
-        type: String,
-    },
-    description: {
-        type: String,
-        default: "The description is of me should be here!",
-    },
-    age: {
-        type: Number,
-    },
-    createdAt: {
-        type: Date,
-        default: new Date(),
-    },
+	name: {
+		type: String,
+		required: [true, "Your name is required"],
+	},
+	email: {
+		type: String,
+		required: [true, "Your email address is required"],
+		unique: true,
+	},
+	// username: {
+	//     type: String,
+	//     required: [true, "Your username is required"],
+	// },
+	password: {
+		type: String,
+		required: [true, "Your password is required"],
+	},
+	telephone: {
+		type: String,
+	},
+	gender: {
+		type: String,
+	},
+	description: {
+		type: String,
+		default: "The description is of me should be here!",
+	},
+	age: {
+		type: Number,
+	},
+	createdAt: {
+		type: Date,
+		default: new Date(),
+	},
 });
 
 userSchema.pre("save", async function () {
-    this.password = await bcrypt.hash(this.password, 12);
+	this.password = await bcrypt.hash(this.password, 12);
 });
 
 module.exports = mongoose.model("User", userSchema);
--- frontend/src/Components/Profile/ProfileContacts.js
+++ frontend/src/Components/Profile/ProfileContacts.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios"; // You may need to install axios if you haven't already
+import axios from "axios";
 import serverUrl from "../../utils/config";
 
 const ProfileContacts = () => {
@@ -19,11 +19,12 @@ const ProfileContacts = () => {
 
 	return (
 		<div>
-			{/* <h1>Contact List</h1> */}
-			<ul className="contact_list">
-				{users.map((user, index) => (
-					<li className="list_item" key={index}>
-						<span>{user.name}</span>
+			{/* <h2>User List</h2> */}
+			<ul>
+				{users.map((user) => (
+					<li key={user._id}>
+						Name: {user.name}
+						{/* Add other user properties as needed */}
 					</li>
 				))}
 			</ul>

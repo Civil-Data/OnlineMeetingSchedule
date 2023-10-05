9645a77a14dddfda9b46fed064ed811ec2307b79
Martin Nilsson <martin.no.nilsson@gmail.com>
2023-10-05 13:48:47 +0200

    ADD: Eslint config file

--- frontend/src/Components/NavigationBar.js
+++ frontend/src/Components/NavigationBar.js
@@ -4,63 +4,53 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { useUserContext, useUpdateUserContext } from "../contexts/LoginContext";
 
 const NavigationBar = () => {
-    const { loginStatus, user } = useUserContext();
-    const { updateLoginStatus } = useUpdateUserContext();
+	const { loginStatus, user } = useUserContext();
+	const { updateLoginStatus } = useUpdateUserContext();
 
-    const iconSize = "27px";
+	const iconSize = "27px";
 
-    return (
-        <div id="navigationBar">
-            <div id="left">
-                {loginStatus && (
-                    <>
-                        <Link
-                            className="nav-buttons"
-                            to="/profile"
-                            type="button"
-                        >
-                            <AccountCircleIcon
-                                fontSize="large"
-                                sx={{ color: "#daa520", height: iconSize }}
-                                titleAccess="Profile Page"
-                            />
-                        </Link>
-                        <Link
-                            className="nav-buttons"
-                            to="/booking"
-                            type="button"
-                        >
-                            <AddIcon
-                                fontSize="large"
-                                sx={{ color: "#daa520", height: iconSize }}
-                                titleAccess="Add Booking"
-                            />
-                        </Link>
-                    </>
-                )}
-            </div>
-            {loginStatus ? (
-                <div id="navButtons" className="flex-se right">
-                    <span id="logged_in">{user.name}</span>
+	return (
+		<div id="navigationBar">
+			<div id="left">
+				{loginStatus && (
+					<>
+						<Link className="nav-buttons" to="/profile" type="button">
+							<AccountCircleIcon
+								fontSize="large"
+								sx={{ color: "#daa520", height: iconSize }}
+								titleAccess="Profile Page"
+							/>
+						</Link>
+						<Link className="nav-buttons" to="/booking" type="button">
+							<AddIcon
+								fontSize="large"
+								sx={{ color: "#daa520", height: iconSize }}
+								titleAccess="Add Booking"
+							/>
+						</Link>
+					</>
+				)}
+			</div>
+			{loginStatus ? (
+				<div id="navButtons" className="flex-se right">
+					<span id="logged_in">{user.name}</span>
 
-                    <Link className="nav-buttons" to="/" type="button">
-                        <span onClick={() => updateLoginStatus(false)}>
-                            Log out
-                        </span>
-                    </Link>
-                </div>
-            ) : (
-                <div id="navButtons" className="flex-se right">
-                    <Link className="nav-buttons" to="/register" type="button">
-                        <i>Sign up</i>
-                    </Link>
-                    <Link className="nav-buttons" to="/login" type="button">
-                        <span>Login</span>
-                    </Link>
-                </div>
-            )}
-        </div>
-    );
+					<Link className="nav-buttons" to="/" type="button">
+						<span onClick={() => updateLoginStatus(false)}>Log out</span>
+					</Link>
+				</div>
+			) : (
+				<div id="navButtons" className="flex-se right">
+					<Link className="nav-buttons" to="/register" type="button">
+						<i>Sign up</i>
+					</Link>
+					<Link className="nav-buttons" to="/login" type="button">
+						<span>Login</span>
+					</Link>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default NavigationBar;

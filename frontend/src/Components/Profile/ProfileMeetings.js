3cb9d293aa4b2c358e6c2c460a4b89c467b01787
Martin Nilsson <martin.no.nilsson@gmail.com>
2023-10-12 01:46:02 +0200

    FIX: Page should now be rendered asyncronusly together with fetch requests. ADD: MeetingItem now renders most of its contents dynamically

--- frontend/src/Components/Profile/ProfileMeetings.js
+++ frontend/src/Components/Profile/ProfileMeetings.js
@@ -3,22 +3,13 @@ import MeetingItem from "./MeetingItem";
 import axios from "axios";
 import { v4 as uuidv4 } from "uuid";
 
-// import { LoginProvider, useUserContext } from "../../contexts/LoginContext";
-// import { useMeetingUpdate } from "../../contexts/MeetingContext";
 import { useUserContext } from "../../contexts/LoginContext";
 import serverUrl from "../../utils/config";
 
-const GetMeetings = async (user) => {
-	// console.log(user);
+const GetMeetings = async user => {
 	try {
-		// const meetings = await axios.get(serverUrl + `/meeting/${user._id}`);
-		const { data } = await axios.get(
-			serverUrl + `/meeting/users?paramName=${user._id}`
-		);
+		const { data } = await axios.get(serverUrl + `/meeting/users?paramName=${user._id}`);
 
-		// const meetings = await axios.get(serverUrl + "/meeting/users", {
-		// 	params: user._id,
-		// });
 		return data;
 	} catch (error) {
 		console.error(error);
@@ -28,26 +19,37 @@ const GetMeetings = async (user) => {
 
 //Component for Meeting overview
 const ProfileMeetings = () => {
-	// const { user } = useUserContext();
 	const { user } = useUserContext();
 	const [meetings, setMeetings] = useState([{}]);
-	// const meetings = useMeetingUpdate();
+	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
-		GetMeetings(user).then((meeting) => {
-			setMeetings(meeting);
-		});
+		const fetchMeetings = async () => {
+			try {
+				const meeting = await GetMeetings(user);
+				setMeetings(meeting);
+			} catch (error) {
+				console.error("Error fetching meetings", error);
+			} finally {
+				setIsLoading(false);
+			}
+		};
+		fetchMeetings();
 	}, [user]);
 
 	return (
 		<>
 			<div className="profile_container">
-				<div className="top_section">
-					{meetings.map((meeting) => {
+				{/* <div className="top_section"> */}
+				{!isLoading ? (
+					meetings.map(meeting => {
 						return <MeetingItem key={uuidv4()} meeting={meeting} />;
-					})}
-				</div>
+					})
+				) : (
+					<></>
+				)}
 			</div>
+			{/* </div> */}
 		</>
 	);
 };

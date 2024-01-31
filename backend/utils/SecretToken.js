d9842df02b0d502e3d8f11237c6415eb323a6fc7
JoelScarinius <anjo19go@student.ju.se>
2024-01-31 12:48:55 +0100

    Structure for user finished

--- backend/utils/SecretToken.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const jwt = require("jsonwebtoken");
-
-// Define a function to create a secret token for a user
-module.exports.createSecretToken = (id) => {
-	return jwt.sign({ id }, process.env.TOKEN_KEY, {
-		expiresIn: 3 * 24 * 60 * 60,
-	});
-};

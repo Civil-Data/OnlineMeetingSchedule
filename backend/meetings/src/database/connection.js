2e8c35e2b5d4e1b2b68fad4bad8bf7eb6ea06cc1
Joel Scarinius <joel.scarinius@pair.se>
2024-01-31 19:44:51 +0100

    FIX: bcrypt to bcryptjs

--- backend/meetings/src/database/connection.js
+++ backend/meetings/src/database/connection.js
@@ -1,21 +1,16 @@
 const mongoose = require("mongoose");
 const { DB_URL } = require("../config");
 
-module.exports = async() => {
-
+module.exports = async () => {
 	try {
 		await mongoose.connect(DB_URL, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
-			useCreateIndex: true
+			useCreateIndex: true,
 		});
 		console.log("Db Connected");
-        
 	} catch (error) {
 		console.error("Error ============ ON DB Connection");
 		console.log(error);
 	}
- 
 };
-
- 

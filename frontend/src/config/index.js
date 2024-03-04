3d82fcef57c48c09882a20f6a3c91977b9ae4aa6
joelScarinius <anjo19go@student.ju.se>
2024-03-04 12:22:21 +0100

    FIX: api-calls

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,6 +1,6 @@
 const SERVER_PORT = 80;
 
-const SERVER_URL = `http://80:${SERVER_PORT}`;
+const SERVER_URL = `http://108.141.254.180:${SERVER_PORT}`;
 // const SERVER_URL = `http://20.54.200.99:${SERVER_PORT}`;
 
 module.exports = {
